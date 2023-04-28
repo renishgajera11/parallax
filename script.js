23d7df74bd596fa20edb99e303d6689459e9845f
renishgajera11 <renishgajerapragetx@gmail.com>
2023-04-28 18:23:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const parallax_el = document.querySelectorAll(".parallax");
+
+let xValue = 0, 
+    yValue = 0;
+
+
+window.addEventListener("mousemove",(e)=>{
+
+
+    xValue=e.clientX - window.innerWidth / 2;
+    yValue=e.clientY - window.innerHeight / 2;
+
+    console.log(xValue, yValue);
+
+parallax_el.forEach((el) => {
+
+    
+    let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+    let zValue = (e.clientX - parseFloat(getComputedStyle(el).left))*isInLeft*0.1
+
+    let speedx = el.dataset.speedx;
+    let speedy = el.dataset.speedy;
+    let speedz = el.dataset.speedz;
+
+    el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${-yValue* speedy}px)) perspective(2300px) translateZ(${zValue*speedz}px)`;
+});
+
+});
+
